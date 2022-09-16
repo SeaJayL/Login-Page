f3986182eadd5c8fc1098cb0d0d8e8c0ed07f522
SeaJayL <101975558+SeaJayL@users.noreply.github.com>
2022-09-16 15:30:52 +0800

    Add files via upload

---
+++ login (updated)/login-info.js
@@ -0,0 +1,26 @@
+const logButton = document.getElementById('button');
+    logButton.onclick = () => {
+        const userDiv = document.querySelector('#username').value;
+        const passDiv = document.querySelector('#password').value;
+    
+            if(!userDiv){
+                console.log('Input Username');
+            }
+            else if(userDiv.length < 3){
+                alert('Username should be longer than 3 characters!');
+            }
+
+            if(!passDiv){
+                console.log('Input Password');
+            }
+            else if(passDiv.length < 3){
+                alert('Username should be longer than 3 characters!');
+            }
+
+            if(userDiv === "admin" && passDiv === "root"){
+                location.href = "login-home.html"
+            }
+            else{
+                alert('Wrong username or password');
+            }
+    }
